e0d41c1bbf88c90dc125876533068d1c8711eb15
OrtRBLX <36776641+OrtRBLX@users.noreply.github.com>
2018-03-01 16:40:12 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,3 @@
-console.log('start =)')
 const discord = require('discord.js');
 const client = new discord.Client();
 
@@ -17,51 +16,73 @@ function isCommand(command, message){
 
 function GiveRole(Role,message){
 	message.member.addRole(Role)
-	
-	message.author.send('**Hooray!** :tada:')
-	message.author.send('You have successfully been given the '+Role.name+' role, new chats should be visible in the "Games" section of the discord. If there are any problems be sure to message an admin or a moderator.')
+	message.author.send('**Hooray!** :tada: \nYou have successfully been given the '+Role.name+' role, new chats should be visible in the "Games" section of the discord. If there are any problems be sure to message an admin or a moderator.')
+	message.delete()
 }
 
 function RemoveRole(Role,message){
-	message.member.addRole(Role)
-	
-	message.author.send('**Hooray!** :tada:')
-	message.author.send('You have successfully been given the '+Role.name+' role, new chats should be visible in the "Games" section of the discord. If there are any problems be sure to message an admin or a moderator.')
+	message.member.removeRole(Role)
+	message.author.send('**Role Removed** :frowning: \nYou have successfully been removed from the '+Role.name+' role, any chats relevent to this role should no longer be visible. If there are any problems be sure to messange an admin or a moderator.')
+	message.delete()
 }
 
 client.on('message', (message) => {
 	if (message.author.bot) return;
     var args = message.content.split(/[ ]+/)
-    if(isCommand('Join', message)){
+    if(isCommand('join', message)){
     	var Channel = args[1];
     	if (Channel){
     		if (message.channel.id==='416623657578790932'){
-    			if (Channel==='arma'){
+    			if (Channel.toLowerCase()==='arma'){
 	    			var Role=message.guild.roles.find('name','Arma')
 	    			GiveRole(Role,message)
-	    		}else if (Channel==='csgo'){
+	    		}else if (Channel.toLowerCase()==='csgo'){
 	    			var Role=message.guild.roles.find('name','CS;GO')
 	    			GiveRole(Role,message)
-	    		}else if (Channel==='rs6'){
+	    		}else if (Channel.toLowerCase()==='rs6'){
 	    			var Role=message.guild.roles.find('name','Rainbow Six Seige')
 	    			GiveRole(Role,message)
-	    		}else if (Channel==='pubg'){
+	    		}else if (Channel.toLowerCase()==='pubg'){
 	    			var Role=message.guild.roles.find('name','PUBG')
 	    			GiveRole(Role,message)
+	    		}else{
+
 	    		}
 	    		
     		}
     		
     	} else {
-    		message.author.send("Please enter a channel name")
+    		message.author.send("Please use role commands in #role-bot and ensure you are using a valid channel name.")
+    		message.delete()
     	}
-    	return;
-    }else if (isCommand('Leave', message)){
-    	message.author.send("leave")
-    }else if (isCommand('Help', message)){
-    	message.author.send("help")
+    }else if (isCommand('leave', message)){
+    	var Channel = args[1];
+    	if (Channel){
+    		if (message.channel.id==='416623657578790932'){
+    			if (Channel.toLowerCase()==='arma'){
+	    			var Role=message.guild.roles.find('name','Arma')
+	    			RemoveRole(Role,message)
+	    		}else if (Channel.toLowerCase()==='csgo'){
+	    			var Role=message.guild.roles.find('name','CS;GO')
+	    			RemoveRole(Role,message)
+	    		}else if (Channel.toLowerCase()==='rs6'){
+	    			var Role=message.guild.roles.find('name','Rainbow Six Seige')
+	    			RemoveRole(Role,message)
+	    		}else if (Channel.toLowerCase()==='pubg'){
+	    			var Role=message.guild.roles.find('name','PUBG')
+	    			RemoveRole(Role,message)
+	    		}
+	    		
+    		}
+    		
+    	} else {
+    		message.author.send("Please use role commands in #role-bot and ensure you are using a valid channel name.")
+    		message.delete()
+    	}
+    }else if (isCommand('info', message)){
+    	message.author.send("__**Welcome to the Big Deek Club's Discord server!**__\n\nThis bot is used to give roles to individuals to be able to view a variety of game channels which are hidden by the default roles. To be able to view any of these game channels follow the insutrctions in #role-bot which show a list of all game channels in the discord.")
+    	message.delete()
     }
-    message.delete()
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login(BOT_TOKEN);
